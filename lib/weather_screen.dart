8a0c20e3bc856624d7bce28a1b3119bd8af07faa
sm4rtz-exe <thanawat7cg@gmail.com>
2026-08-25 21:00:11 +0700

    primary card

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
@@ -21,14 +22,42 @@ class WeatherScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          //main card
-          Placeholder(fallbackHeight: 250.00),
-          SizedBox(height: 20),
-          //weather forecast card
-          Placeholder(fallbackHeight: 150.00),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            //main card
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 20,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Temp",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 32,
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      Icon(Icons.cloud, size: 64),
+                      Text("weather", style: TextStyle(fontSize: 20)),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            //weather forecast card
+            Placeholder(fallbackHeight: 150.00),
+            SizedBox(height: 20),
+            //additional information
+            Placeholder(fallbackHeight: 150.00),
+          ],
+        ),
       ),
     );
   }
