be204c4ba12c2da7bac39416861bf380e37c186d
sm4rtz-exe <thanawat7cg@gmail.com>
2026-08-25 18:15:07 +0700

    appbar and refresh button

---
+++ lib/weather_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class WeatherScreen extends StatelessWidget {
+  const WeatherScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Weather App",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        actions: [
+          GestureDetector(
+            child: Icon(Icons.refresh),
+            onTap: () {
+              print("refresh");
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
